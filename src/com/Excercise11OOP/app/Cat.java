fa3a3ede07f9b87eb7d5e4641f39affef61ab0ec
akain88 <larahaze88@gmail.com>
2019-10-16 15:58:29 -0500

    cat class created

---
+++ src/com/Excercise11OOP/app/Cat.java
@@ -0,0 +1,11 @@
+package com.Excercise11OOP.app;
+
+public class Cat {
+
+	public static void main(String[] args) 
+	{
+		//
+
+	}
+
+}
--- src/com/Excercise11OOP/app/OOP.java
+++ src/com/Excercise11OOP/app/OOP.java
@@ -10,6 +10,11 @@ public class OOP {
 	
 		//way 2
 		Perro tribilin=new Perro();
+		
+		System.out.println("pluto´s age:"+pluto.age);
+		System.out.println("pluto's height:"+pluto.Height);
+		System.out.println("pluto's"+pluto.id);
+		
 		pluto.age=15;
 		pluto.Height=50;
 		pluto.id=10;
@@ -17,6 +22,11 @@ public class OOP {
 		System.out.println("pluto´s age:"+pluto.age);
 		System.out.println("pluto's height:"+pluto.Height);
 		System.out.println("pluto's"+pluto.id);
+		
+		Perro firulais=new Perro(1,"Firullais","labrador",5,60.0);
+		System.out.println("Firulais age:"+firulais.age);
+		System.out.println("Firulais height:"+firulais.Height);
+		System.out.println("Firulais id:"+firulais.id);
 
 	}
 
